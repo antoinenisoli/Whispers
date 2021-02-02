7a0d412477ab50e40c00a17a7df3e48999193221
antoinenisoli <nisoliantoine@outlook.com>
2021-02-02 10:25:48 +0100

    new ending

--- Assets/Scripts/UI/GameOverAnimation.cs
+++ Assets/Scripts/UI/GameOverAnimation.cs
@@ -10,11 +10,15 @@ public class GameOverAnimation : MonoBehaviour
     [SerializeField] Image fadeScreen;
     [SerializeField] Text endText;
     [SerializeField] float fadeDuration = 5;
+    [SerializeField] Image finalImage;
+    [SerializeField] Sprite[] frames;
 
     private void Start()
     {
         EventManager.instance.onEndGame.AddListener(EndGame);
         fadeScreen.DOFade(0, 0);
+        finalImage.DOFade(0, 0);
+        finalImage.sprite = frames[0];
         endText.DOFade(0, 0);
     }
 
@@ -22,9 +26,21 @@ public class GameOverAnimation : MonoBehaviour
     {
         yield return new WaitForSeconds(5f);
         fadeScreen.DOFade(1, fadeDuration);
-        endText.DOFade(1, fadeDuration).SetDelay(1);
 
-        yield return new WaitForSeconds(fadeDuration + 3);
+        yield return new WaitForSeconds(fadeDuration/2);
+        finalImage.DOFade(1, 3);
+
+        yield return new WaitForSeconds(5);
+        finalImage.DOFade(0, 2);
+
+        yield return new WaitForSeconds(3.5f);
+        finalImage.sprite = frames[1];
+        finalImage.DOFade(1, 2);
+
+        yield return new WaitForSeconds(5);
+        endText.DOFade(1, 2.5f);
+
+        yield return new WaitForSeconds(5f);
         SceneManager.LoadScene(0);
     }
 
